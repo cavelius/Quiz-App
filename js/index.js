c19b4b6c21a08ffdf74bfbb9d5572537ef0e3f95
Sofia Cavelius <sofiacavelius@web.de>
2023-12-18 15:05:33 +0100

    add Toggle functionality

--- js/index.js
+++ js/index.js
@@ -4,7 +4,10 @@ const bookmarkcontainer = document.querySelector(
   '[data-js="bookmark-container"]'
 );
 const bookmarkPicture = document.querySelector('[data-js="bookmarkPicture"]');
+const btn = document.querySelector('[data-js="btn"]');
+const answer = document.querySelector('[data-js="answer"]');
 
+console.log(btn);
 console.log(bookmarkPicture);
 console.log(bookmarkcontainer);
 
@@ -18,4 +21,20 @@ bookmarkcontainer.addEventListener("click", () => {
     bookmarkPicture.src = "css/img/bookmark_filled.png";
   }
   questionCardBookmarked = !questionCardBookmarked;
+});
+
+// answerbtn
+
+let answerIsHide = false;
+
+btn.addEventListener("click", () => {
+  console.log("btn is clicked");
+  answer.classList.toggle("answerVisible");
+
+  if (answerIsHide) {
+    btn.textContent = "show answer";
+  } else {
+    btn.textContent = "hide answer";
+  }
+  answerIsHide = !answerIsHide;
 });
